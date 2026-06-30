08bc0af0a8dbc0e7b2f9c04916a37fbbc916ab6a
jhesser333 <jhesser333@gmail.com>
2026-06-30 10:50:10 -0400

    Move tutorial text to vertical center of screen (in front of Morph)

--- src/TutorialText.jsx
+++ src/TutorialText.jsx
@@ -2,7 +2,7 @@ export default function TutorialText({ text, visible }) {
   return (
     <div style={{
       position: 'absolute',
-      top: '63%', bottom: 16, left: 0, right: 0,
+      top: 0, bottom: 0, left: 0, right: 0,
       display: 'flex', alignItems: 'center', justifyContent: 'center',
       pointerEvents: 'none',
       padding: '0 80px',
